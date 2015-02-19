e584cf29861df9e4d0dc5c3cbbdccf65551e6f77
johnehill <johnehill@users.noreply.github.com>
2015-02-19 13:52:13 -0600

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'johnehill.l8mm4g9e'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1Ijoiam9obmVoaWxsIiwiYSI6Im9pSXZsNTAifQ.NzslAM2ZOO6Z-W54LDSdDw'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
